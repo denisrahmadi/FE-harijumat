55a376f2257fdb0bb13a82267b606a01a9147aeb
Kusuma-io <109591510+Kusuma-io@users.noreply.github.com>
2022-10-02 16:50:50 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+// const { create } = require("domain");
+const http = require("http");
+const app = require("./app");
+
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+server.listen(port);
